68fb475dc81efbd46ed7411b5b31022ec4b28184
Japa Dev AI <angelo.matsui@outlook.com>
2025-03-15 17:51:04 -0300

    Ajustes

--- start.sh
+++ start.sh
@@ -8,7 +8,3 @@ code-server --auth none --bind-addr 0.0.0.0:8080 /home/vscode &
 
 # Mantém o contêiner em execução
 tail -f /dev/null
-
-
-  git config --global user.email "angelo.matsui@outlook.com"
-  git config --global user.name "japa-dev-ai"
